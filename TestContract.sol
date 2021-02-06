01db5a883a451af4d70990e516ab892cdc1c2685
Marc Droogh <33723117+mdroogh@users.noreply.github.com>
2021-02-06 14:39:21 +0100

    Poly sol (#11)
    * Initial commit
    
    * Add initial test and solve contract
    
    * Add initial test and solve contract
    
    * Set ownership of contracts
    
    * Set ownership of contracts
    
    * Transfer value after solving tests
    
    * Transfer value after solving tests
    
    * Add deploy example
    
    * Add deploy example
    
    * Transfer ether to hunter
    
    * Transfer ether to hunter
    
    * Add final version of example contract
    
    * Add final version of example contract
    
    * Add removed files
    
    * Add removed files
    
    * Add refund method
    
    * Add refund method
    
    * Update readme with instructions
    
    * Update code comments
    
    * Add comments to deploy file
    
    * Edit comments after feedback
    
    Co-authored-by: a-t-0 <34750068+a-t-0@users.noreply.github.com>

---
+++ TestContract.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+// Example contract of a TestContract.
+contract TestContract {
+
+    bool solved;    // Boolean to denote if contract is solved.
+    address payable owner;  // Owner of the contract, first this is the sponser.
+    uint expiry;        // Get the time when the contract expires.
+
+    constructor() payable {      // Constructor to initialise values.
+        solved = false;         //  Boolean value to indicate if contract is already solved.
+        owner = msg.sender;     //  Set the owner of the contract to the creator of the contract.
+        expiry = 1612569800;    //  Unix timestamp of the moment of expiry. 
+    }
+
+    function test(address payable hunter) public payable {
+        TemplateSolveContract solver = TemplateSolveContract(msg.sender); // The message sender is the contract activating the test function.
+        uint x = 100;                                   // Sample input.
+        uint16 y = 10;                                  // Sample expected output.
+        require(y == solver.main(x), "Wrong output");   // Require the output of the main function to be y.
+        solved = true;                                  // Set solved to true.
+        owner = hunter;                                 // Set the ownership to the hunter.
+        owner.transfer(address(this).balance);          // Transfer the bounty to the hunter.
+    }
+
+    // Getter function for the solved variable.
+    function getSolved() public view returns (bool){   
+        return solved;
+    }
+    
+    // Getter function for the Ownership.
+    function getOwner() public view returns (address) { 
+        return owner;
+    }
+    
+    // Getter function for the balance of the contract.
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    
+    // Refund method to claim the value of the contract after expiry.
+    function refund() public {
+        require(msg.sender == owner && block.timestamp >= expiry, "Contract is not expired yet");   // The sender must own the contract and the contract must be expired.
+        selfdestruct(owner);    // Let the contract selfdestruct and move the value to the owner.
+    }
+
+}
+
+// TemplateSolveContract so the TestContract knows the structure of the SolveContract.
+abstract contract TemplateSolveContract {
+    function main(uint x) virtual public returns (uint);
+}
---
+++ deploy.js
@@ -0,0 +1,33 @@
+// This file can be used to deploy an ethereum contract to a local ethereum blockchain.
+// The following tutorial was used:
+// https://hackernoon.com/how-to-set-up-your-own-ethereum-development-environment-a-step-by-step-guide-j5kp3b55
+const fs = require('fs');
+const Web3 = require('web3');
+const web3 = new Web3('http://localhost:8545');
+
+// get / retrieve required bytecode and abi for the contract from a local file, these files should be created after compiling the solidity files.
+const bytecode = fs.readFileSync('./build/TestContract.bin');
+const abi = JSON.parse(fs.readFileSync('./build/TestContract.abi'));
+
+(async function () {
+  // Set the owner of the contract.
+  const ganacheAccounts = await web3.eth.getAccounts();
+  const myWalletAddress = ganacheAccounts[0];
+
+  // Initialise the contract with the abi.
+  const myContract = new web3.eth.Contract(abi);
+
+  // Deploy the contract.
+  myContract.deploy({
+    data: bytecode.toString(),
+  }).send({
+    from: myWalletAddress,
+    gas: 5000000
+  }).then((deployment) => {
+    console.log('FirstContract was successfully deployed!');
+    console.log('FirstContract can be interfaced with at this address:');
+    console.log(deployment.options.address);
+  }).catch((err) => {
+    console.error(err);
+  });
+})();
