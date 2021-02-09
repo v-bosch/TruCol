b7d588e80f07255433c6ba87f3bec42f06c4bc04
v-bosch <51364448+v-bosch@users.noreply.github.com>
2021-02-09 11:51:00 +0100

    Created example N-Queens Contracts (#12)
    * n_queens contracts (unfinished)
    
    * actual code, verification test unfinished
    
    * function n-queens contracts
    
    * file name change
    
    * remove solveNQ
    
    * added comments for clarity

---
+++ SolveNQueens.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+// This contract contains a solution to the N-queens problem that the "testNQUeens" bounty contract has presented. 
+
+contract SolveNQueens {
+
+    TemplateTestContract testContract;  // Create variable for the testContract which needs to be solved.
+    address payable owner;      
+    bool[][]board;              // Chessboard
+    uint8 N;                    // Number of queens  
+
+    constructor(address testAddress) payable {              
+        testContract = TemplateTestContract(testAddress);   // Initialise the testContract variable.
+        owner = msg.sender;                                 // Initialise the owner of the contract to be the creator of the contract.
+        N = 4;
+    }
+
+     // Function to solve the problem specified in the testContract.
+    function solve() public payable{
+        testContract.test(owner);
+    }
+
+    function main(bool[][] memory unsolved_board) public returns (bool[][] memory solved_board) {
+        (bool a, bool[][] memory solution) = solveNQ(x, N);
+        return(solution);
+
+    }
+
+    // general solve function that is called by main, takes the empty board as input and returns whether there 
+    // is a solution, and the solution itself. 
+
+    function solveNQ(bool[][] memory board_in, uint8 col) public returns (bool, bool[][] memory) { 
+        // If all queens are placed 
+        if (board_in[0].length == N) 
+            return (true, board_in); 
+    
+        for (uint8 i = 0; i < N; i++) { 
+            // Check if the queen can be placed 
+            if (isSafe(board_in, i, col)) { 
+                // Place queen 
+                board[i][col] = true; 
+    
+                // recur for the next column
+                (bool a, bool[][] memory c) = solveNQ(board_in, col + 1);
+                if (a)
+                    return (true, c); 
+    
+                // no queen present, so set bool of this square to false
+                board[i][col] = false; 
+            } 
+        } 
+        // If the queen cannot be placed in any row in 
+        //this colum col then return false 
+        return (false, board_in); 
+    }
+
+    // function that checks the squares on the board, in order to find a safe position for the queen 
+    function isSafe(bool[][] memory b, uint8 row, uint8 col) public view returns (bool) { 
+        uint8 i = row;
+        uint8 j = col; 
+    
+        // Check whether there is a queen in the row on left side 
+        for (i = 0; i < col; i++) 
+            if (b[row][i]) 
+                return false; 
+    
+        // Check whether there is a queen in the upper diagonal on left side 
+        for (i;i >= 0; i--) 
+            for (j; j >= 0; j--) 
+                if (b[i][j]) 
+                    return false; 
+        
+        // Check whether there is a queen in the lower diagonal on left side 
+        for (i; i < N; i++) 
+            for (j; j >= 0; j--) 
+                if (b[i][j]) 
+                    return false; 
+    
+        return (true); 
+    }     
+}
+
+// TemplateTestContract so the SolveContract knows the structure of the testContract.
+abstract contract TemplateTestContract {
+    function test(address payable hunter) virtual external;
+}
---
+++ testNQueens.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+// This contract sets out the bounty for a simple N-Queens problem (with N=4), to be solved by the "SolveNQueens" contract.
+
+contract TestNQueens {
+
+    bool[][]board;              // Chessboard
+    uint8 N;                    // Number of queens  
+    
+    bool solved;                // Boolean to denote if contract is solved.
+    address payable owner;      // Owner of the contract, first this is the sponser.
+    uint expiry;                // Get the time when the contract expires.
+    
+    constructor() payable {         // Constructor to initialise values.
+            solved = false;         //  Boolean value to indicate if contract is already solved.
+            owner = msg.sender;     //  Set the owner of the contract to the creator of the contract.
+            expiry = 1612569800;    //  Unix timestamp of the moment of expiry. 
+        }
+
+
+    function test(address payable hunter) public payable {
+        TemplateSolveContract solver = TemplateSolveContract(msg.sender);
+        
+        // Initialize the empty chessboard that needs to be solved
+        board = [[ false, false, false, false ], 
+        [ false, false, false, false ], 
+        [false, false, false, false], 
+        [ false, false, false, false]];
+        
+        require(checkQueens(solver.main(board)), "Wrong answer");
+       
+        solved = true;                                  // Set solved to true.
+        owner = hunter;                                 // Set the ownership to the hunter.
+        owner.transfer(address(this).balance);          // Transfer the bounty to the hunter.  
+    }
+    
+     function checkQueens(bool[][] memory b) public view returns(bool correct) {
+        uint8 queens;
+        uint8 [] memory rowqueens;
+        uint8 [] memory colqueens;
+        // check for every square whether there is a queen (value is true), if there is, add one to the variables.
+        for (uint8 i = 0; i < N; i++) { 
+            for (uint8 j = 0; j < N; j++) {
+                if (b[i][j] == true)
+                    queens ++;
+                    rowqueens[i] ++;
+                    colqueens[j] ++;
+                    // if there are more queens than specified, or there is more than 1 queen in a row/col, the solution is incorrect.
+                    if (queens > N || rowqueens[i] > 1 || colqueens[j] > 1)
+                        return false;
+            }
+        }
+        return true;
+    }
+
+    // Getter function for the solved variable.
+    function getSolved() public view returns (bool){   
+        return solved;
+    }
+    
+    // Getter function for the Ownership.
+    function getOwner() public view returns (address) { 
+        return owner;
+    }
+    
+    // Getter function for the balance of the contract.
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    
+    // Refund method to claim the value of the contract after expiry.
+    function refund() public {
+        require(msg.sender == owner && block.timestamp >= expiry, "Contract is not expired yet");   // The sender must own the contract and the contract must be expired.
+        selfdestruct(owner);    // Let the contract selfdestruct and move the value to the owner.
+    }
+}
+
+// TemplateSolveContract so the TestContract knows the structure of the SolveContract.
+abstract contract TemplateSolveContract {
+    function main(bool[][] memory x) virtual public returns (bool[][] memory);
+}
