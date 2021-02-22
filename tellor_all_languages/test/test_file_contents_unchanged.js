2b051323c0c6a8b81fdd0ad1f79df453bcca3f63
a-t-0 <eai@protonmail.com>
2021-02-22 13:46:50 +0100

    Refactored file content test.

--- tellor_all_languages/test/test_file_contents_unchanged.js
+++ tellor_all_languages/test/test_file_contents_unchanged.js
@@ -27,20 +27,9 @@ contract("UsingTellor Tests", function (accounts) {
 	// 0. the bounty hunter github name
 	// 1. The repository name belonging to the sponsor contract
     	
-	// -----------------------------------------Simulate API Call of Tellor Oracles ----------------------------
-	// Read whether a travis build has failed or passed
-	// v-bosch/sponsor_example - no build yet for v-bosch/sponsor_example
-	// a-t-0/taskwarrior-installation - failed
-	// a-t-0/shell_unit_testing_template - passed
-	const github_username_hunter = "a-t-0"
-	const github_username_sponsor = "a-t-0"
-	const repo_name_hunter = "sponsor_example"
-	const repo_name_sponsor = "sponsor_example"
-	const branch_hunter = "main"
-	const branch_sponsor = "main"
-		
-	// Encode a string such as "passed" to a number 
-	// Source: https://stackoverflow.com/questions/12740659/downloading-images-with-node-js
+    	
+	// -----------------------------------------Helper Functions ----------------------------
+	// Encode a string to a number 
 	// Source: https://stackoverflow.com/questions/14346829/is-there-a-way-to-convert-a-string-to-a-base-10-number-for-encryption
 	function encode(string) {
 		var number = "0x";
@@ -48,62 +37,10 @@ contract("UsingTellor Tests", function (accounts) {
 		for (var i = 0; i < length; i++)
 			number += string.charCodeAt(i).toString(16);
 		return number;
-	}
-
-	// Show the contract contains the logic to identify a correct
-	// build fail/pass. If the build passes, a 2 is expected from the contract.
-	// Otherwise a 1 is expected.
-	const passed_test = 2
-	const failed_test = 1
-	
-	// hardcode the expected test results based on which repository is tested.
-	// the taskwarrior-installation has a passed build status, whereas the 
-	// shell_unit_testing_template has a failed build status, both should be
-	// correctly identified by the contract.
-	var expected_contract_output = passed_test
-	
-	// specify travis api command that gets the build status and stores its result in an output file
-	var output_filename = "output.txt"
-	var hunter_filelist_filename = "hunter.txt"
-	var sponsor_filelist_filename = "sponsor.txt"
-	
-	// get the list of files per repo	
-	file_list_hunter_repo = "echo $(curl -X GET https://api.github.com/repos/"+github_username_hunter+"/"+repo_name_hunter+"/git/trees/"+branch_hunter+"?recursive=1) | grep -Po \x27\x22path\x22:.*?[^\\\\]\x22,\x27"
-	
-	file_list_sponsor_repo = "echo $(curl -X GET https://api.github.com/repos/"+github_username_sponsor+"/"+repo_name_sponsor+"/git/trees/"+branch_sponsor+"?recursive=1) | grep -Po \x27\x22path\x22:.*?[^\\\\]\x22,\x27"
-	
-	// Define command per line/file of repo
-	// TODO: First check if the line starts with:> "path": 
-	// Then remove the:"> "path": " part (without the outside double qoutation marks
-	//var git_filepath = "${line:9:-2}" // bad substitution in shell
-	//var git_filepath = "{line:9:-2}"
-	var git_filepath = "$line"
-	
-	
-	var curl_hunter_files = "curl \x22https://raw.githubusercontent.com/"+github_username_hunter+"/"+repo_name_hunter+"/"+branch_hunter+"/"+git_filepath+"\x22"
-	var curl_sponsor_files = "curl \x22https://raw.githubusercontent.com/"+github_username_sponsor+"/"+repo_name_sponsor+"/"+branch_sponsor+"/"+git_filepath+"\x22"
-	// YIELDS VALID COMMAND: 
-	//var command_per_line = "diff <("+curl_hunter_files+") <("+curl_sponsor_files+") > "+output_filename
-	var command_per_line = curl_hunter_files+" > hunter_temp_content.txt"+" && "+curl_sponsor_files+" > sponsor_temp_content.txt && diff hunter_temp_content.txt sponsor_temp_content.txt > "+output_filename
-	//var command_per_line = "diff <("+curl_hunter_files+") <("+curl_sponsor_files+") > "+output_filename
-	console.log("COMMAND PER LINE=")
-	console.log(command_per_line)
-	
-	// Then prepend the github repository curl command to the path that remains
-	var command = "while read line; do "+command_per_line+"; done < sponsor.txt"
-	console.log("cOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOommand=")
-	console.log(command)
-	// WOrks
-	//while read line; do diff <(curl https://raw.githubusercontent.com/a-t-0/sponsor_example/main/${line:9:-2}) <(curl https://raw.githubusercontent.com/a-t-0/sponsor_example/attack_unit_test/${line:9:-2}); done < sponsor.txt
-	//while read line; do diff <(curl "https://raw.githubusercontent.com/a-t-0/sponsor_example/main/${line:9:-2}") <(curl "https://raw.githubusercontent.com/a-t-0/sponsor_example/attack_unit_test/${line:9:-2}") > output.txt; done < sponsor.txt
-	//while read line; do diff <(curl "https://raw.githubusercontent.com/a-t-0/sponsor_example/main/${line:9:-2}") <(curl "https://raw.githubusercontent.com/a-t-0/sponsor_example/attack_unit_test/${line:9:-2}") > output.txt; done < sponsor.txt
-	
-	// New approach
-	// while read line; do curl "https://raw.githubusercontent.com/a-t-0/sponsor_example/main/${line:9:-2}" > hunter_temp_content.txt && curl "https://raw.githubusercontent.com/a-t-0/sponsor_example/attack_unit_test/${line:9:-2}" > sponsor_temp_content.txt && diff hunter_temp_content.txt sponsor_temp_content.txt > output.txt; done < sponsor.txt
-	
-	// run some bash command
+	}    	
+    	
+    // Function that runs some incoming shell command (not bash)
 	const exec = require('child_process').exec;
-
 	function os_func() {
 		this.execCommand = function (cmd) {
 		    return new Promise((resolve, reject)=> {
@@ -119,14 +56,80 @@ contract("UsingTellor Tests", function (accounts) {
 	}
 	var os = new os_func();
 	
+	// retry getting some file
+	function doCall(urlToCall, callback) {
+		urllib.request(urlToCall, { wd: 'nodejs' }, function (err, data, response) {                              
+		    return callback(data);
+		});
+	}
+    	
+
+	// -----------------------------------------Specify Tellor Oracles Data Sources ----------------------------
+	// Read whether a travis build has failed or passed
+	// v-bosch/sponsor_example - no build yet for v-bosch/sponsor_example
+	// a-t-0/taskwarrior-installation - failed
+	// a-t-0/shell_unit_testing_template - passed
+	const github_username_hunter = "a-t-0"
+	const github_username_sponsor = "a-t-0"
+	const repo_name_hunter = "sponsor_example"
+	const repo_name_sponsor = "sponsor_example"
+	const branch_hunter = "main"
+	const commit_hunter = ""
+	const branch_sponsor = "main"
+	const commit_sponsor = ""
+	
+	
+	// -----------------------------------------Specify Temporary input and output (files)------------------------
+	// Show the contract contains the logic to identify a correct build fail/pass. 
+	// If the build passes, a uint256 of value 2 is expected 
+	// from the contract. Otherwise a uint of value 1 is expected.
+	const expected_sponsor_contract_output = 2
+	
+	// Specify local output location of curled data
+	var test_case = "unchanged"
+	var test_type = "file_contents"
+	var output_filename = test_type+"_"+test_case+".txt"
+	
+	// Specify output location of repository file lists
+	// TODO: move into subfolder
+	// TODO: refactor into names that contain test type
+	var hunter_filelist_filename = "hunter.txt"
+	var sponsor_filelist_filename = "sponsor.txt"
+	
+	
+	// -----------------------------------------Specify Curl Commands That Get API Data---------------------------
+	// get the list of files per repo	
+	file_list_hunter_repo = "echo $(curl -X GET https://api.github.com/repos/"+github_username_hunter+"/"+repo_name_hunter+"/git/trees/"+branch_hunter+"?recursive=1) | grep -Po \x27\x22path\x22:.*?[^\\\\]\x22,\x27"
+	
+	file_list_sponsor_repo = "echo $(curl -X GET https://api.github.com/repos/"+github_username_sponsor+"/"+repo_name_sponsor+"/git/trees/"+branch_sponsor+"?recursive=1) | grep -Po \x27\x22path\x22:.*?[^\\\\]\x22,\x27"
+	
+	// Define command per line/file of repo
+	// TODO: First check if the line starts with:> "path": 
+	// Then remove the:"> "path": " part (without the outside double qoutation marks
+	var git_filepath = "$line"
+	
+	var curl_hunter_files = "curl \x22https://raw.githubusercontent.com/"+github_username_hunter+"/"+repo_name_hunter+"/"+branch_hunter+"/"+git_filepath+"\x22"
+	var curl_sponsor_files = "curl \x22https://raw.githubusercontent.com/"+github_username_sponsor+"/"+repo_name_sponsor+"/"+branch_sponsor+"/"+git_filepath+"\x22"
+	var command_per_line = curl_hunter_files+" > hunter_temp_content.txt"+" && "+curl_sponsor_files+" > sponsor_temp_content.txt && diff hunter_temp_content.txt sponsor_temp_content.txt > "+output_filename
+	console.log("COMMAND PER LINE=")
+	console.log(command_per_line)
+	
+	// Then prepend the github repository curl command to the path that remains
+	var command = "while read line; do "+command_per_line+"; done < sponsor.txt"
+	console.log("cOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOommand=")
+	console.log(command)	
+	
 	// create commands to get hunter and sponsor repo file lists
 	var export_hunter_files = file_list_hunter_repo  +" > hunter.txt"
 	var export_sponsor_files = file_list_sponsor_repo  +" > sponsor.txt"
 	
 	// remove artifacts from file lists from repos
+	// TODO: specify in name that it is a command
 	var remove_artifacts_hunter = "sed -i 's/\x22,//g' hunter.txt && sed -i 's/\x22path\x22: \x22//g' hunter.txt"
 	var remove_artifacts_sponsor = "sed -i 's/\x22,//g' sponsor.txt && sed -i 's/\x22path\x22: \x22//g' sponsor.txt"
 	
+	
+	// -----------------------------------------Get The Tellor Oracles Data With Shell --------------------------
 	// export hunter repo file list
 	os.execCommand(export_hunter_files).then(res=> {
 		console.log("Getting list of files in repository of bounty hunter, please wait 10 seconds.", res);
@@ -148,35 +151,6 @@ contract("UsingTellor Tests", function (accounts) {
 		console.log("Getting list of files in repository of sponsor, please wait 10 seconds.", err);
 	})
 	
-	// manually get some file
-	var request = require('request');
-	request.get('https://raw.githubusercontent.com/a-t-0/sponsor_example/main/test_sqrt.py', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-		    var csv = body;
-		    // Continue with your processing here.
-		    console.log("csv")
-		    console.log(csv)
-		    return csv
-		}
-	});
-	//console.log("request")
-	//console.log(request)
-	
-	// retry getting some file
-	function doCall(urlToCall, callback) {
-		urllib.request(urlToCall, { wd: 'nodejs' }, function (err, data, response) {                              
-		    return callback(data);
-		});
-	}	
-	
-	var urlToCall = "https://raw.githubusercontent.com/a-t-0/sponsor_example/main/test_sqrt.py";
-	doCall(urlToCall, function(response){
-		// Here you have access to your variable
-		console.log("AATTEMPT 2")
-		console.log(response.toString());
-		return response.toString()
-	})
-
 	// wait till file is read (it takes a while)
 	// TODO: do not hardcode the build time, but make it dependend on completion of the os_func function. 
 	await new Promise(resolve => setTimeout(resolve, 10000));
@@ -201,7 +175,6 @@ contract("UsingTellor Tests", function (accounts) {
 		console.log("Computing the difference between the list of files in the repos of the sponsor and bounty hunter, please wait 10 seconds.", err);
 	})
 	
-	
 	// Retry bash command
 	console.log("REEEEEEEEETRY")
 	// Source: https://stackoverflow.com/questions/37732331/execute-bash-command-in-node-js-and-get-exit-code
@@ -215,7 +188,6 @@ contract("UsingTellor Tests", function (accounts) {
 	dir.on('exit', function (code) {
 	  // exit code is code
 	});
-	
 
 	// wait till file is read (it takes a while)
 	// TODO: do not hardcode the build time, but make it dependend on completion of the os_func function. 
@@ -241,6 +213,6 @@ contract("UsingTellor Tests", function (accounts) {
 	// -----------------------------------------Verify the contract returns the correct retrieved value ----------------------------
     await tellorOracle.submitValue(requestId, mockValue);
     let retrievedVal = await compareFileListsInRepo.readTellorValue(requestId);
-	assert.equal(retrievedVal.toString(), expected_contract_output);
+	assert.equal(retrievedVal.toString(), expected_sponsor_contract_output);
   });
 });
